8c0701c2f5447700bc9a60709bfd8cd39cd05ad5
vamsinanduru25 <73776106+vamsinanduru25@users.noreply.github.com>
2022-07-01 18:26:19 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Int from './Interface.js';
+
+class App extends React.Component{
+  render(){
+    return(
+      <div >
+        <Int/>
+      </div>
+    );
+    
+  }
+}
+
+export default App;
---
+++ Interface.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Style.css";
+import "./img1.png";
+const Int =() => {
+    return(
+        <div>
+        
+            <img className="img1" src="img1.png"/>
+            <p className="t1">The Avengers<br/>EndGame</p>
+        </div>
+    );
+
+}
+export default Int;
